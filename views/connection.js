deed225e9f7dc7ae0786e258d90a44e329fff4f0
Ivo Dilov <iddilov@gmail.com>
2017-08-30 00:17:32 +0300

    view for battles

--- views/connection.js
+++ views/connection.js
@@ -3,6 +3,7 @@ var players, indForPid;
 var teams, indForTid;
 var battles, indForBid;
 var judge = 0, admin = 0;
+var day = 0;
 
 socket.on('init', (competitionName, _players, _indForPid, _teams, _indForTid, _battles, _indForBid)=>{
     setUp(competitionName);
@@ -14,6 +15,8 @@ socket.on('init', (competitionName, _players, _indForPid, _teams, _indForTid, _b
     indForBid = _indForBid;
     sortTeams();
     redoResults();
+    sortBattles();
+    redoBattles();
 });
 
 function sortTeams(){
@@ -25,6 +28,24 @@ function sortTeams(){
                 indForTid[teams[j].id] = j;
             }
         }
+    }
+}
+function sortBattles(){
+    for (let i=0; i<battles.length; ++i){
+        for (let j=battles.length-1; j>i; --j){
+            if (battles[j].day < battles[j-1].day){
+                [battles[j], battles[j-1]] = [battles[j-1], battles[j]];
+                indForBid[battles[j-1].id] = j-1;
+                indForBid[battles[j].id] = j;
+            }
+        }
+    }
+    if (battles.length != 0){day = battles[battles.length-1].day;}
+}
+function isDayActive(){
+    for (let i=battles.length-1; i>=0; --i){
+        if (battles[i].day != day){return 0;}
+        if (battles[i].points1 == 0 && battles[i].points2 == 0){return 1;}
     }
 }
 
--- views/view.js
+++ views/view.js
@@ -28,6 +28,16 @@ function teamTR(ind){
     for (let i=0; i<td.length; ++i){tr.appendChild(td[i]);}
     return tr;
 }
+function battleTR(ind){
+    let tr = document.createElement('tr');
+    let td = [document.createElement('td'), document.createElement('td'), document.createElement('td'), document.createElement('td')];
+    td[0].appendChild(document.createTextNode(teams[indForTid[battles[ind].team1]].name));
+    td[1].appendChild(document.createTextNode(battles[ind].points1));
+    td[2].appendChild(document.createTextNode(battles[ind].points2));
+    td[3].appendChild(document.createTextNode(teams[indForTid[battles[ind].team2]].name));
+    for (let i=0; i<td.length; ++i){tr.appendChild(td[i]);}
+    return tr;
+}
 
 function redoResults(){
     let results = document.getElementById("results");
@@ -36,6 +46,46 @@ function redoResults(){
     }
     for (let i=0; i<teams.length; ++i){
         results.appendChild(teamTR(i));
+    }
+}
+
+function redoBattles(){
+    let battlesDom = document.getElementById("block_battles");
+    while(battlesDom.firstChild){
+        battlesDom.removeChild(battlesDom.firstChild);
+    }
+    let j = battles.length-1;
+    for (let i = day; i>0; --i){
+        let elem = document.createElement('h2');
+        elem.appendChild(document.createTextNode("Ден "+i));
+        battlesDom.appendChild(elem);
+        let table = document.createElement('table');
+        battlesDom.appendChild(table);
+        table.setAttribute("class", "table table-bordered");
+        table.style.textAlign = "center";
+        table.style.maxWidth = "800px";
+        //table.style.margin = "auto";
+        let head = document.createElement('thead');
+        let row = document.createElement('tr');
+        elem = document.createElement('td');
+        elem.appendChild(document.createTextNode("Отбор"));
+        row.appendChild(elem);
+        elem = document.createElement('td');
+        elem.appendChild(document.createTextNode("Резултат"));
+        elem.colSpan = 2;
+        row.appendChild(elem);
+        elem = document.createElement('td');
+        elem.appendChild(document.createTextNode("Отбор"));
+        row.appendChild(elem);
+        head.appendChild(row);
+        table.appendChild(head);
+        let body = document.createElement('tbody');
+        table.appendChild(body);
+        
+        while(j>=0 && battles[j].day == i){--j;}
+        for (let k=j+1; k<battles.length && battles[k].day == i; ++k){
+            body.appendChild(battleTR(k));
+        }
     }
 }
 
